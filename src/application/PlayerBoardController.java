b0c47f5f7b2c91533db6701b700817871c8db93f
Sam Durm <samdurm@iu.edu>
2022-10-28 11:44:02 -0400

    Updating

--- src/application/PlayerBoardController.java
+++ src/application/PlayerBoardController.java
@@ -1,5 +1,34 @@
 package application;
 
-public class PlayerBoardController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
+public class PlayerBoardController {
+    Boolean[][] bombsBoard = new Boolean[10][10];
+    
+    Boolean[][] revealedBoard = new Boolean[10][10];
+    
+    @FXML
+    private BorderPane mainLayout;
+	
+	// ----- Create GridPane -----// 
+	GridPane tiles = new GridPane(); 
+	
+	public void addTiles() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				// ----- Buttons/Tiles -----// 
+				Button buttonTemp = new Button("  "); 
+				tiles.add(buttonTemp, i, j);
+				buttonTemp.setId("" + i + ", "+ j + "");
+				System.out.println("" + i + j + " ID: " + buttonTemp.getId());
+			}
+		}
+	}
+
+	
+
+	
 }
