f3680fe3d9795b225671e05ba651a346b6f09531
Sam Durm <samdurm@iu.edu>
2022-11-11 17:25:26 -0500

    Fixing testfailure.java

--- src/application/TestFailure.java
+++ src/application/TestFailure.java
@@ -1,40 +1,49 @@
 package application;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
-
-import org.junit.After;
-import org.junit.Before;
-
-import junit.framework.TestCase;
-
 
 
 class TestFailure {
-	 
-	//Generate accurate number of mines
-	@Test
-	void testTenMines() {
-		Board playerBoard = new Board(); 
-		playerBoard.addMines();
-		
-		int numMines = playerBoard.mines.size();
-		
-		assertEquals(10, numMines);
-	}
-	
-	
-	@Test
-	void testTotalCells() {
-		Board tiles = new Board(); 
-		test.addTiles();
-		int totalCells = test.tiles.impl_getColumnCount();
-		int totalCells2 = test.tiles.impl_getRowCount();
-		
-		assertEquals(100.0, totalCells);
-		
-	}
-	
+    // Generate accurate number of mines
+    @Test
+    void testTenMines() {
+        PlayerBoardController controller = new PlayerBoardController(); 
+        controller.addMines();
+        
+        int numMines = controller.mines.size();
 
+        assertEquals(10, numMines);
+    }
+    
+    @Test
+    void testNewMine() {
+        PlayerBoardController controller = new PlayerBoardController(); 
+        controller.addMines();
+        
+        ArrayList<String> generatedMines = controller.mines;
+        
+        Set<String> mySet = new HashSet<String>(generatedMines);
+        assertEquals(mySet.size(), generatedMines.size());    
+    }
+    
+//	Could not get the following tests to work 
+//    @Test
+//    void testCellCount() {
+//        PlayerBoardController controller = new PlayerBoardController(); 
+//        controller.addTiles();
+//        int numCells = controller.totalTiles;
+//        assertEquals(100, numCells);
+//    }
+//    
+//    @Test
+//    void testCells() {
+//        PlayerBoardController controller = new PlayerBoardController(); 
+//        controller.addTiles();
+//        boolean empty = Board.newInstance.getBombsBoard().getChildren().isEmpty();
+//        assertEquals(false, empty);    
+//    }   
 }
